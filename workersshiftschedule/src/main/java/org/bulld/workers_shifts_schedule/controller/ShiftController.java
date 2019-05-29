1134f5242a17595b1a39e6feb3e214569cd3f465
Potapov Roman <potapovroma555@gmail.com>
2019-05-29 12:01:59 +0400

    Changed ShiftController method getShiftByDate mapping and return value to ResponseEntity Added standard getShiftById method and getShiftByEmployeeId(is it correct to work with EmployeeRepository in ShiftController to solve Foreign Key problem?) Corresponding changes to ShiftRepository added

--- workersshiftschedule/src/main/java/org/bulld/workers_shifts_schedule/controller/ShiftController.java
+++ workersshiftschedule/src/main/java/org/bulld/workers_shifts_schedule/controller/ShiftController.java
@@ -2,7 +2,9 @@ package org.bulld.workers_shifts_schedule.controller;
 
 
 import org.bulld.workers_shifts_schedule.exception.ResourceNotFoundException;
+import org.bulld.workers_shifts_schedule.model.Employee;
 import org.bulld.workers_shifts_schedule.model.Shift;
+import org.bulld.workers_shifts_schedule.repository.EmployeeRepository;
 import org.bulld.workers_shifts_schedule.repository.ShiftRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,13 +22,35 @@ public class ShiftController {
     @Autowired
     private ShiftRepository shiftRepository;
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     @GetMapping("/shifts")
     public List<Shift> getAllShifts() {
         return shiftRepository.findAll();
     }
 
-    @GetMapping("/shifts/{date}")
-    public List<Shift> getShiftsByDate(@PathVariable(value = "date") String shiftDate){return shiftRepository.findAllByDate(LocalDate.parse(shiftDate));}
+    @GetMapping("/shifts/{id}")
+    public ResponseEntity<Shift> getShiftById(@PathVariable(value = "id") Long shiftId)
+            throws ResourceNotFoundException {
+        Shift shift = shiftRepository.findById(shiftId)
+                .orElseThrow(() -> new ResourceNotFoundException("Shift not found for this id :: " + shiftId));
+        return ResponseEntity.ok().body(shift);
+    }
+
+    @GetMapping("/shifts/shifts_by_date/{date}")
+    public ResponseEntity<List<Shift>> getShiftsByDate(@PathVariable(value = "date") String shiftDate) throws ResourceNotFoundException{
+        List<Shift> shifts = shiftRepository.findAllByDate(LocalDate.parse(shiftDate))
+                .orElseThrow(() -> new ResourceNotFoundException("Shifts not found for this date :: " + shiftDate));
+        return ResponseEntity.ok().body(shifts);
+    }
+
+    @GetMapping("/shifts/shifts_by_employee_id/{employee_id}")
+    public ResponseEntity<List<Shift>> getShiftsByEmployeeId(@PathVariable(value = "employee_id") Long employeeId) throws ResourceNotFoundException{
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+        return ResponseEntity.ok().body(shiftRepository.findAllByEmployee(employee));
+    }
 
     @PostMapping("/shifts")
     public Shift createShift(@Valid @RequestBody Shift shift) {
--- workersshiftschedule/src/main/java/org/bulld/workers_shifts_schedule/repository/ShiftRepository.java
+++ workersshiftschedule/src/main/java/org/bulld/workers_shifts_schedule/repository/ShiftRepository.java
@@ -1,13 +1,16 @@
 package org.bulld.workers_shifts_schedule.repository;
 
+import org.bulld.workers_shifts_schedule.model.Employee;
 import org.bulld.workers_shifts_schedule.model.Shift;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ShiftRepository extends JpaRepository<Shift, Long> {
-    List<Shift> findAllByDate (LocalDate date);
+    Optional<List<Shift>> findAllByDate (LocalDate date);
+    List<Shift> findAllByEmployee (Employee employee);
 }
